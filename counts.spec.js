678bed8a32c2654ed7a02e01a54008d378d4da74
Lee Goddard <leegee@gmail.com>
2018-12-15 19:03:57 +0100

    init

---
+++ counts.spec.js
@@ -0,0 +1,38 @@
+// Data structure
+// {
+//     red: new Array(256), // Count of the number of times a value appears in the red channel
+//     green: new Array(256), // Count of the number of times a value appears in the green channel
+//     blue: new Array(256), // Count of the number of times a value appears in the blue channel
+//     alpha: new Array(256), // Count of the number of times a value appears in the alpha channel
+
+//     colors: {
+//         rgb: 0, // Number of unique RGB colors
+//         rgba: 0 // Number of unique RGBA colors
+//     },
+
+//     palettes: {
+//         rgb: [], // Array of unique colors in hex notation
+//         rgba: [] // Array of unique colors in hexa notation
+//     },
+
+//     greyscale: true, // Indicates whether all colors are greyscale or not
+//     alphachannel: false // Indicates that one or more pixels are translucent
+// }
+
+
+const expect = require('chai').expect;
+const getHsls = require('./getHsls');
+
+const fileName = '../../Pictures/illy-card.jpg';
+
+describe('getHsl', () => {
+    it('counts', () => {
+        getHsls(fileName).then( hsls => {
+            const sorted = hsls.sort( (a, b) => a.count < b.count );
+            const entries = Object.keys(sorted);
+            expect( entries.length, 'Total colors').to.equal(11541);
+            console.log( sorted[entries[0]] );
+            console.log( sorted[entries[entries.length - 1]] );
+        });
+    });
+})
---
+++ getHsls.js
@@ -0,0 +1,56 @@
+const conversions = require('./color-conversions');
+const canvasPrebuilt = require('canvas-prebuilt');
+const Canvas = canvasPrebuilt.Canvas;
+const Image = canvasPrebuilt.Image;
+
+const getCanvasImageData = async (imageBuffer) => {
+  return new Promise( (resolve, reject) => {
+    const canvasImage = new Image();
+    canvasImage.crossOrigin = 'Anonymous';
+
+    canvasImage.onerror = function () {
+      return reject( new Error('error reading from input stream') );
+    };
+
+    canvasImage.onload = function () {
+      const ctx = new Canvas(canvasImage.width, canvasImage.height).getContext('2d');
+      ctx.drawImage(canvasImage, 0, 0);
+      resolve(
+        ctx.getImageData(0, 0, canvasImage.width, canvasImage.height).data
+      );
+    };
+
+    canvasImage.src = imageBuffer;
+  });
+};
+
+module.exports = async (imageBuffer) => {
+  if (['string', 'object'].indexOf(typeof imageBuffer) === -1) {
+      throw new TypeError('Expected imageBuffer to be a string or a buffer');
+  }
+
+  const data = await getCanvasImageData(imageBuffer);
+  
+  const hsls = {};
+
+  for (let i = 0; i < data.length; i += 4) {
+    const red   = data[i];
+    const green = data[i + 1];
+    const blue  = data[i + 2];
+    const alpha = data[i + 3];
+
+    const hsl = conversions.rgbToHsl(red, green, blue);
+    const key = hsl.join(',');
+
+    if (hsls.hasOwnProperty(key)) {
+      hsls[key].count ++
+    } else {
+      hsls[key] = {
+        count: 1,
+        hsl
+      };
+    }
+  }
+
+  return Object.values(hsls);
+};
